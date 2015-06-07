13a21841758f55eceac146eb2d7cc1d4c1bbdbb1
dlwndghk94 <joonghwalee@joongs-air.gateway.pace.com>
2015-06-06 19:41:30 -0700

    cname update

--- mailer.php
+++ mailer.php
@@ -34,7 +34,7 @@ $message
 mail($myemail, $subject, $message);
 
 /* Redirect visitor to the thank you page */
-header('Location: http://address-of-confirmation-page.html');
+header('Location: http://sceneusamarketinggroup.com/contact-success.html');
 exit();
 
 /* Functions we used */
